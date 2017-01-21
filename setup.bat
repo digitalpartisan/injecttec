02727dfe312c28c2a02c76d5cfb694cec2c72104
Joel <joel.flake@gmail.com>
2017-01-20 20:27:19 -0800

    Added .bat files

---
+++ setup.bat
@@ -0,0 +1,3 @@
+mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\InjectTecTesting.esp" "K:\FO4 Mods\InjectTecTesting\Data\InjectTecTesting.esp"
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\InjectTecTesting" "K:\FO4 Mods\InjectTecTesting\Data\Scripts\InjectTecTesting"
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\InjectTecTesting" "K:\FO4 Mods\InjectTecTesting\Data\Scripts\Source\User\InjectTecTesting"
---
+++ teardown.bat
@@ -0,0 +1,2 @@
+del /q "K:\Steam\steamapps\common\Fallout 4\Data\InjectTecTesting.esp"
+rmdir /s /q "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\InjectTecTesting" "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\InjectTecTesting"
