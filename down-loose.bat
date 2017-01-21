ffadeb4548ea726982f3303665a47973d84327d0
Joel <joel.flake@gmail.com>
2017-01-20 21:58:11 -0800

    enabled separate up/down control of loose and packed files

--- down-loose.bat
+++ down-loose.bat
@@ -1,3 +1,1 @@
-del /q "K:\Steam\steamapps\common\Fallout 4\Data\InjectTec - Main.ba2"
-del /q "K:\Steam\steamapps\common\Fallout 4\Data\InjectTec.esp"
 rmdir /s /q "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\InjectTec" "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\InjectTec"
---
+++ down-packed.bat
@@ -0,0 +1,1 @@
+del /q "K:\Steam\steamapps\common\Fallout 4\Data\InjectTec - Main.ba2"
